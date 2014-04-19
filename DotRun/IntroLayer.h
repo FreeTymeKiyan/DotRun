166a3d85376b238fb37a0c6efcd65890910482f4
Kiyan Liu <Kiyan@Kiyans-MacBook-Pro.local>
2014-04-19 19:36:39 -0400

    Initial Commit

---
+++ DotRun/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  DotRun
+//
+//  Created by Kiyan Liu on 4/19/14.
+//  Copyright FreeTymeKiyan 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ DotRun/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DotRun
+//
+//  Created by Kiyan Liu on 4/19/14.
+//  Copyright FreeTymeKiyan 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
